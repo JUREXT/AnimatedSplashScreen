48c26eb4befdea81e97a1a15dee438778de1caf8
JureLokovsek <jurext@hotmail.com>
2020-01-04 02:59:33 +0100

    Initial setup.

---
+++ app/src/main/java/com/lokovsek/animatedsplashscreen/MainActivity.java
@@ -0,0 +1,22 @@
+package com.lokovsek.animatedsplashscreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.text) TextView text;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        text.setText("Testing");
+    }
+}
